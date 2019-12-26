821402fda2f7dd4bd891b388b3265d69fd3e8935
Stanislav <stas.yeremitsa@gmail.com>
2019-12-26 23:03:21 +0100

    1st

---
+++ src/App_XML_GUI/Applic_XML.java
@@ -0,0 +1,25 @@
+package App_XML_GUI;
+
+import javax.swing.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Vector;
+import java.util.logging.*;
+
+public class Applic_XML
+{
+    private JPanel defaultPanel;
+    private JLabel jLabel1;
+
+
+}
